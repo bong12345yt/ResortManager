b00be6423e0ec9844f1fb17baa26f0f940d4d968
duongban <duongduban11@gmail.com>
2019-09-29 17:06:25 +0700

    Add BUS DAO GiaoDich

---
+++ ResortManagerDTO/DTO/GiaoDich.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResortManagerDTO.DTO
+{
+    public class GiaoDich
+    {
+        public String MaDoan { get; set; }
+
+        public int SoNguoi { get; set; }
+
+        public int SoPhong { get; set; }
+
+        public DateTime NgayBatDau { get; set; }
+
+        public DateTime NgayKetThuc { get; set; }
+
+        public int TongTien { get; set; }
+
+        public String CMND { get; set; }
+
+        public String TrangThai { get; set; }
+    }
+}
